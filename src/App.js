db0839748f043291d746ed710623cb3f065d81bd
Rachael <rachael.rocha@mavs.uta.edu>
2023-07-26 22:42:37 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
-import React, { useState } from 'react';
-import Question from './Question';
-import image from './images/C1Logo.png'
-
+import "./App.css";
+import React, { useState } from "react";
+import Question from "./Question";
+import image from "./images/C1Logo.png";
 
 function App() {
-
   //keep track of the current question index
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [isAnswerCorrect, setIsAnswerCorrect] = useState(null);
@@ -15,32 +13,46 @@ function App() {
     setIsAnswerCorrect(isCorrect);
   };
 
-   //clicking the next question
+  //clicking the next question
   const handleNextQuestion = () => {
-  setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
-  setIsAnswerCorrect(null); // Reset the isAnswerCorrect state
+    setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+    setIsAnswerCorrect(null); // Reset the isAnswerCorrect state
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={image} className="App-logo" alt="logo" />
-        
-        <div className='question-section'>
-          <div className='question-count'>
-            <span>{`Question ${currentQuestionIndex + 1}/${Question.length}`}</span>
+        <div className="Title">Financial One Literacy</div>
+        <div className="question-section">
+          <div className="question-count">
+            <span>{`Question ${currentQuestionIndex + 1}/${
+              Question.length
+            }`}</span>
           </div>
-          <div className='question-text'>{currentQuestion.question}</div>
-         <div className='is-correct'>
+          <div className="question-text">{currentQuestion.question}</div>
+          <div className="is-correct">
             {isAnswerCorrect !== null && (
-              <span>{isAnswerCorrect ? "Correct" : "Incorrect"}</span>
+              <span>
+                {isAnswerCorrect ? (
+                  <h2>Correct</h2>
+                ) : (
+                  <>
+                    Incorrect<p></p>
+                  </>
+                )}
+              </span>
             )}
           </div>
         </div>
-        <div className='answer-section'>
+        <div className="answer-section">
           {currentQuestion.answers.map((answer, index) => (
-            <button 
-              className={`Button ${isAnswerCorrect !== null && answer.isCorrect ? 'correct' : ''} ${isAnswerCorrect === false && !answer.isCorrect ? 'incorrect' : ''}`}
+            <button
+              className={`${
+                isAnswerCorrect !== null && answer.isCorrect
+                  ? "correct"
+                  : "incorrect"
+              } Button`}
               key={index}
               onClick={() => handleAnswerClick(answer.isCorrect)}
               disabled={isAnswerCorrect !== null}
@@ -49,15 +61,15 @@ function App() {
             </button>
           ))}
         </div>
-        {isAnswerCorrect !== null && currentQuestionIndex < Question.length - 1 && (
-          <button className= 'next-question' onClick={handleNextQuestion}>Next Question</button>
-        )}
+        {isAnswerCorrect !== null &&
+          currentQuestionIndex < Question.length - 1 && (
+            <button className="next-question" onClick={handleNextQuestion}>
+              Next Question
+            </button>
+          )}
       </header>
     </div>
   );
-  
-
-  
 }
 
 export default App;
