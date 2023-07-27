7aaab00212d79e2c89a761f467fd0dd3c30d8426
Rachael <rachael.rocha@mavs.uta.edu>
2023-07-26 20:18:49 -0500

    df

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Question from './Question';
+import image from './images/C1Logo.png'
+
 
 function App() {
+
+  //keep track of the current question index
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [isAnswerCorrect, setIsAnswerCorrect] = useState(null);
+  const currentQuestion = Question[currentQuestionIndex];
+
+  const handleAnswerClick = (isCorrect) => {
+    setIsAnswerCorrect(isCorrect);
+  };
+
+   //clicking the next question
+  const handleNextQuestion = () => {
+  setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+  setIsAnswerCorrect(null); // Reset the isAnswerCorrect state
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={image} className="App-logo" alt="logo" />
+        <div className='question-section'>
+          <div className='question-count'>
+            <span>{`Question ${currentQuestionIndex + 1}/${Question.length}`}</span>
+          </div>
+          <div className='question-text'>{currentQuestion.question}</div>
+         <div className='is-correct'>
+            {isAnswerCorrect !== null && (
+              <span>{isAnswerCorrect ? "Correct" : "Incorrect"}</span>
+            )}
+          </div>
+        </div>
+        <div className='answer-section'>
+          {currentQuestion.answers.map((answer, index) => (
+            <button 
+              className={`Button ${isAnswerCorrect !== null && answer.isCorrect ? 'correct' : ''} ${isAnswerCorrect === false && !answer.isCorrect ? 'incorrect' : ''}`}
+              key={index}
+              onClick={() => handleAnswerClick(answer.isCorrect)}
+              disabled={isAnswerCorrect !== null}
+            >
+              {answer.text}
+            </button>
+          ))}
+        </div>
+        {isAnswerCorrect !== null && currentQuestionIndex < Question.length - 1 && (
+          <button onClick={handleNextQuestion}>Next Question</button>
+        )}
       </header>
     </div>
   );
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={image} className="App-logo" alt="logo" />
+  //       <div className='question-section'>
+  //         <div className='question-count'>
+  //           <span>{`Question ${currentQuestionIndex + 1}/${Question.length}`}</span>
+  //         </div>
+  //         <div className='question-text'>{currentQuestion.question}</div>
+  //       </div>
+  //       <div className='answer-section'>
+  //         {currentQuestion.answers.map((answer, index) => (
+  //           <button className='Button' key={index}>{answer.text}</button>
+  //         ))}
+  //       </div>
+  //       {currentQuestionIndex < Question.length - 1 && (
+  //         <button onClick={handleNextQuestion}>Next Question</button>
+  //       )}
+  //     </header>
+  //   </div>
+  // );
+
+  
 }
 
 export default App;
---
+++ src/Question.js
@@ -0,0 +1,122 @@
+// import React from 'react'
+
+// isCorrect Function
+// question array
+const Question = [
+    {
+        id: 1,
+        question: "What is ?",
+        // answer object array // this array will be shuffled 
+        answers: [
+            {
+                // Choice 1 //
+                text: "right",
+                isCorrect: true,
+                correctDescription: "",
+                incorrectDescription: ""
+            },
+            {
+                // Choice 2 //
+                text: "fda",
+                isCorrect: false,
+                correctDescription: "",
+                incorrectDescription: ""
+            },
+            {
+                // Choice 3 //
+                text: "dfa",
+                isCorrect: false,
+                correctDescription: "",
+                incorrectDescription: ""
+            },
+            {
+                // Choice 4 //
+                text: "fads",
+                isCorrect: false,
+                correctDescription: "",
+                incorrectDescription: ""
+            }
+
+
+        ]
+
+    },
+    {
+        id: 2,
+        question: "How can you ....?",
+        // answer object array // this array will be shuffled 
+        answers: [
+            {
+                // Choice 1 //
+                text: "Dog",
+                isCorrect: false,
+                correctDescription: "",
+                incorrectDescription: ""
+            },
+            {
+                // Choice 2 //
+                text: "Cat",
+                isCorrect: false,
+                correctDescription: "",
+                incorrectDescription: ""
+            },
+            {
+                // Choice 3 //
+                text: "fdfd",
+                isCorrect: false,
+                correctDescription: "",
+                incorrectDescription: ""
+            },
+            {
+                // Choice 4 //
+                text: "hi",
+                isCorrect: true,
+                correctDescription: "",
+                incorrectDescription: ""
+            }
+
+
+        ]
+
+    },
+    {
+        id: 1,
+        question: "What is fdafsd?",
+        // answer object array // this array will be shuffled 
+        answers: [
+            {
+                // Choice 1 //
+                text: "wrong",
+                isCorrect: false,
+                correctDescription: "",
+                incorrectDescription: ""
+            },
+            {
+                // Choice 2 //
+                text: "wrong",
+                isCorrect: false,
+                correctDescription: "",
+                incorrectDescription: ""
+            },
+            {
+                // Choice 3 //
+                text: "right",
+                isCorrect: true,
+                correctDescription: "",
+                incorrectDescription: ""
+            },
+            {
+                // Choice 4 //
+                text: "wrong",
+                isCorrect: false,
+                correctDescription: "",
+                incorrectDescription: ""
+            }
+
+
+        ]
+
+    }
+]
+
+export default Question
